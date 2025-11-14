32ca42585465740ea3db97426eff470b8dd28f4e
José Donoso <j.donoso.catalan@gmail.com>
2025-11-14 10:29:54 -0300

    Actividad 1: Inicialización de proyecto Maven con dependencias de pruebas

---
+++ src/main/java/com/examen/examen/automatizacion/ExamenAutomatizacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.examen.examen.automatizacion;
+
+/**
+ *
+ * @author josedonoso
+ */
+public class ExamenAutomatizacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
